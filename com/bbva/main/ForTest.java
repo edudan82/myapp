2e3a97cd3416028afe9ef5f5602dc71ba99a5e52
augus <edudan82@gmail.com>
2024-12-30 18:32:06 -0600

    prueba de for y de if

---
+++ com/bbva/main/ForTest.java
@@ -0,0 +1,14 @@
+package com.bbva.main;
+
+public class ForTest {
+
+    public static void main(String[] args) {
+        var valorABuscar=10;
+
+        if(valorABuscar==10){
+            System.out.println("numero igual a 10");
+
+        }
+    }
+
+}
---
+++ com/bbva/main/IfTest.java
@@ -0,0 +1,14 @@
+package com.bbva.main;
+
+public class IfTest {
+
+    public static void main(String[] args) {
+        var valorABuscar=10;
+
+        if(valorABuscar==10){
+            System.out.println("numero igual a 10");
+
+        }
+    }
+
+}
